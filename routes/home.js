5985a4d6bee13dd152aa83c3e758be59e1bc9fea
Pecherskiy88 <pecherskiy88@gmail.com>
2020-07-06 16:53:01 +0300

    added routes

---
+++ routes/home.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const router = Router();
+
+router.get('/', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  // с движком теперь так:
+  res.render('index', {
+    title: 'Главная страница',
+    isHome: true,
+  });
+});
+
+module.exports = router;
