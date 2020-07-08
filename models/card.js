e9fdd75355949082f4e1783be6c9d2a545d4881d
Pecherskiy88 <pecherskiy88@gmail.com>
2020-07-09 00:21:44 +0300

    module3 complete

--- models/card.js
+++ models/card.js
@@ -32,6 +32,33 @@ class Card {
       });
     });
   }
+
+  static async remove(id) {
+    const card = await Card.fetch();
+
+    const idx = card.courses.findIndex((c) => c.id === id);
+    console.log('idx: ', idx);
+    const course = card.courses[idx];
+
+    if (course.count === 1) {
+      // удалить
+      card.courses = card.courses.filter((c) => c.id !== id);
+    } else {
+      // изменить кол-во
+      card.courses[idx].count--;
+    }
+    card.price -= course.price;
+
+    return new Promise((resolve, reject) => {
+      fs.writeFile(p, JSON.stringify(card), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(card);
+        }
+      });
+    });
+  }
   static async fetch() {
     return new Promise((resolve, reject) => {
       fs.readFile(p, 'utf-8', (err, content) => {
