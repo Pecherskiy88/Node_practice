7aefc164cc24a0d7d1687fd346cee87e9ac3a5d1
Pecherskiy88 <pecherskiy88@gmail.com>
2020-07-13 15:19:13 +0300

    did populate and ref in BD

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const cardRoutes = require('./routes/card');
 
+const User = require('./models/user');
+
 const app = express();
 
 app.engine(
@@ -26,6 +28,16 @@ app.engine(
 
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5f0c210b8e220c9928756ee4');
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log('req.user Error: ', e);
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
@@ -46,6 +58,16 @@ async function start() {
       useNewUrlParser: true,
       useFindAndModify: false,
     });
+
+    const candidate = await User.findOne();
+    if (!candidate) {
+      const user = new User({
+        email: 'pecherskiy88@gmail.com',
+        name: 'Artem',
+        cart: { items: [] },
+      });
+      await user.save();
+    }
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
--- routes/add.js
+++ routes/add.js
@@ -11,11 +11,11 @@ router.get('/', (req, res) => {
 
 router.post('/', async (req, res) => {
   const { title, price, img } = req.body;
-  // const course = new Course(title, price, img);
   const course = new Course({
     title,
     price,
     img,
+    userId: req.user._id,
   });
 
   try {
