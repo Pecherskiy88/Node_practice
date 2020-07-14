546f5b15ec7a4fb22bc026f220e28c75241f0752
Pecherskiy88 <pecherskiy88@gmail.com>
2020-07-14 12:50:27 +0300

    orders page almost done

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const cardRoutes = require('./routes/card');
+const ordersRoutes = require('./routes/orders');
 
 const User = require('./models/user');
 
@@ -46,6 +47,7 @@ app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
+app.use('/orders', ordersRoutes);
 
 const PORT = process.env.PORT || 3000;
 
