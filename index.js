1e91f86f9c0131ac4ab0d09ee46b371e7575ac8b
Pecherskiy88 <pecherskiy88@gmail.com>
2020-07-14 21:37:46 +0300

    start to do module-5

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const cardRoutes = require('./routes/card');
 const ordersRoutes = require('./routes/orders');
+const authRoutes = require('./routes/auth');
 
 const User = require('./models/user');
 
@@ -48,6 +49,7 @@ app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
 app.use('/orders', ordersRoutes);
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
