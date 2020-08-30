294ea14252ad283304f1968d8be9e9c5878c8eef
Pecherskiy88 <pecherskiy88@gmail.com>
2020-08-30 14:32:00 +0300

    finished module 7

--- index.js
+++ index.js
@@ -18,9 +18,12 @@ const coursesRoutes = require('./routes/courses');
 const cardRoutes = require('./routes/card');
 const ordersRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
 
 const varMiddleware = require('./middleware/variables');
 const userMiddleware = require('./middleware/user');
+const errorHandler = require('./middleware/error');
+const fileMiddleware = require('./middleware/file');
 
 const keys = require('./keys');
 
@@ -45,6 +48,7 @@ app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
@@ -54,6 +58,7 @@ app.use(
     store,
   }),
 );
+app.use(fileMiddleware.single('avatar'));
 app.use(csrf());
 app.use(flash());
 app.use(varMiddleware);
@@ -65,7 +70,9 @@ app.use('/courses', coursesRoutes);
 app.use('/card', cardRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
+app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
 
 async function start() {
